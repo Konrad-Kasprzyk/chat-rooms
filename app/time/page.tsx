a388a8541def16cbbf4fb12237b4cdc8367588de
Konrad Kasprzyk <konradwp2135@gmail.com>
2023-01-05 14:11:29 +0100

    Add some unimportant pages for testing

---
+++ app/time/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { addDoc, collection, getDoc, serverTimestamp, Timestamp } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../db/firebase";
+import collections from "../../global/constants/collections";
+
+export default function Time() {
+  async function getServerTime() {
+    const docRef = await addDoc(collection(db, collections.projects), {
+      title: "first project",
+      currentTime: serverTimestamp() as Timestamp,
+    });
+    const projectSnap = await getDoc(docRef);
+    const data = projectSnap.data();
+    setServerTime(data!.currentTime.toDate());
+  }
+  const [time, setTime] = useState(new Date());
+  const [serverTime, setServerTime] = useState(new Date());
+
+  return (
+    <main>
+      <h1>Checking time</h1>
+      <div>normal{time.toString()}</div>
+      <div>ISO string{time.toISOString()}</div>
+      <div>UTC string{time.toISOString()}</div>
+      <button
+        onClick={() => {
+          setTime(new Date());
+        }}
+      >
+        Set current time
+      </button>
+      <br />
+      <div>server time {serverTime.toString()}</div>
+      <button onClick={() => getServerTime()}>get server time</button>
+    </main>
+  );
+}
