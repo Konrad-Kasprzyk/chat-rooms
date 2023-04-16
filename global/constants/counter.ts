e3b8e62712e87916b2c4942fad31a6a0a43175c1
Konrad Kasprzyk <konradwp2135@gmail.com>
2023-04-16 21:44:18 +0200

    Major init constants update

--- global/constants/counter.ts
+++ global/constants/counter.ts
@@ -1,10 +1,10 @@
-export const INIT_USER_SHORT_ID = "0";
-// search id is number of item: 1, 2, .., 10, 11. It is for tagging tasks, goals etc.
+export const INIT_USER_SHORT_ID = " ";
+
 export const INIT_TASK_SEARCH_ID = 1;
-// short id is a-z A-Z 0-9 chars. It is for finished task Stats
-export const INIT_TASK_SHORT_ID = "0";
-export const INIT_TASK_LABEL_SHORT_ID = "6";
-export const INIT_TASK_COLUMN_SHORT_ID = "5";
+export const INIT_TASK_SHORT_ID = " ";
+
+export const INIT_LABEL_SHORT_ID = "&";
+export const INIT_COLUMN_SHORT_ID = "%";
+
 export const INIT_GOAL_SEARCH_ID = 1;
-export const INIT_GOAL_SHORT_ID = "0";
-export const INIT_GOAL_LABEL_SHORT_ID = "0";
+export const INIT_GOAL_SHORT_ID = " ";
--- global/constants/timeToRemoveFromBin.ts
+++ global/constants/timeToRemoveFromBin.ts
@@ -2,3 +2,5 @@ export const PROJECT_DAYS_IN_BIN = 30;
 export const GOAL_DAYS_IN_BIN = 14;
 export const NORM_DAYS_IN_BIN = 7;
 export const TASK_DAYS_IN_BIN = 7;
+export const LABEL_DAYS_IN_BIN = 7;
+export const COLUMN_DAYS_IN_BIN = 7;
