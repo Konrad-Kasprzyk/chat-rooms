bf4be49942d94b40fa8759ef2149b8dde87e914a
Konrad Kasprzyk <konradwp2135@gmail.com>
2023-04-22 19:09:43 +0200

    Test counter ids generator

---
+++ __tests__/client_api/counterIdsGenerator.test.ts
@@ -0,0 +1,93 @@
+import getNextShortId from "../../global/utils/counterIdsGenerator";
+
+describe("Ids generator works properly", () => {
+  it("asserts id is incremented without adding new char", () => {
+    const currentId = " ";
+    const properNextId = "!";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is incremented with adding new char", () => {
+    const currentId = "~";
+    const properNextId = "  ";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts '\\' id is properly incremented", () => {
+    const currentId = "\\";
+    const properNextId = "]";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is properly incremented into '\\' ", () => {
+    const currentId = "[";
+    const properNextId = "\\";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts '\"' id is properly incremented", () => {
+    const currentId = '"';
+    const properNextId = "#";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is properly incremented into '\"' ", () => {
+    const currentId = "!";
+    const properNextId = '"';
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts ' id is properly incremented", () => {
+    const currentId = "'";
+    const properNextId = "(";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is properly incremented into ''' ", () => {
+    const currentId = "&";
+    const properNextId = "'";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is incremented when changing middle chars and without adding new char", () => {
+    const currentId = "~~~~abc";
+    const properNextId = "    bbc";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+
+  it("asserts id is incremented when changing all chars and adding new char", () => {
+    const currentId = "~~~~";
+    const properNextId = "     ";
+
+    const nextId = getNextShortId(currentId);
+
+    expect(nextId).toEqual(properNextId);
+  });
+});
