90e052b16992a74d7aacb39361428978aa0a6dd8
Konrad Kasprzyk <konradwp2135@gmail.com>
2024-03-14 12:51:23 +0100

    Add the 'page not found' page.

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div
+      className="d-flex justify-content-center"
+      style={{ marginTop: "35vh" }}
+    >
+      <h1>Page not found</h1>
+    </div>
+  );
+}
