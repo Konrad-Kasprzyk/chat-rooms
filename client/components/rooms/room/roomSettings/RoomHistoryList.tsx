bce48a8283ead045948f088f0c2959cea7ec2b6b
Konrad Kasprzyk <konradwp2135@gmail.com>
2024-03-23 11:03:53 +0100

    Add a scroll to top button in history lists.

--- client/components/rooms/room/roomSettings/RoomHistoryList.tsx
+++ client/components/rooms/room/roomSettings/RoomHistoryList.tsx
@@ -1,11 +1,13 @@
 import listenWorkspaceHistoryRecords from "client/api/history/workspaceHistory/listenWorkspaceHistoryRecords.api";
+import backToTopButtonStyles from "client/components/rooms/room/backToTopButton.module.scss";
 import DEFAULT_LARGE_HORIZONTAL_ALIGNMENT from "client/constants/defaultLargeHorizontalAlignment.constant";
 import WorkspaceHistory from "common/clientModels/historyModels/workspaceHistory.model";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import RoomHistoryRecord from "./RoomHistoryRecord";
 
 export default function RoomHistoryList() {
   const [historyRecords, setHistoryRecords] = useState<WorkspaceHistory["history"]>([]);
+  const [hideBackToTopButton, setHideBackToTopButton] = useState(true);
 
   useLayoutEffect(() => {
     const workspaceHistoryRecordsSubscription = listenWorkspaceHistoryRecords().subscribe(
@@ -16,32 +18,59 @@ export default function RoomHistoryList() {
     return () => workspaceHistoryRecordsSubscription.unsubscribe();
   }, []);
 
+  useEffect(() => {
+    function decideIfShowBackToTopButton() {
+      setHideBackToTopButton(window.scrollY < 300);
+    }
+
+    document.addEventListener("scroll", decideIfShowBackToTopButton);
+    return () => document.removeEventListener("scroll", decideIfShowBackToTopButton);
+  }, []);
+
   return (
-    <ul
-      className={`list-group list-group-flush overflow-auto ${DEFAULT_LARGE_HORIZONTAL_ALIGNMENT}`}
-    >
-      {historyRecords.map((historyRecord) => {
-        let oldValue: string | number | null;
-        let value: string | number | null;
-        if (historyRecord.action == "creationTime" || historyRecord.action == "placingInBinTime") {
-          oldValue =
-            historyRecord.oldValue === null ? null : (historyRecord.oldValue as Date).getTime();
-          value = historyRecord.value === null ? null : (historyRecord.value as Date).getTime();
-        } else {
-          oldValue = historyRecord.oldValue as string | null;
-          value = historyRecord.value as string | null;
+    <div>
+      <button
+        className={`btn btn-primary ${backToTopButtonStyles.backToTopButton}`}
+        style={{ display: hideBackToTopButton ? "none" : "block" }}
+        onClick={() =>
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          })
         }
-        return (
-          <RoomHistoryRecord
-            key={historyRecord.id}
-            action={historyRecord.action}
-            actionMakerUsername={historyRecord.user ? historyRecord.user.username : ""}
-            dateMillis={historyRecord.date.getTime()}
-            oldValue={oldValue}
-            value={value}
-          />
-        );
-      })}
-    </ul>
+      >
+        Back to top
+      </button>
+      <ul
+        className={`list-group list-group-flush overflow-auto ${DEFAULT_LARGE_HORIZONTAL_ALIGNMENT}`}
+      >
+        {historyRecords.map((historyRecord) => {
+          let oldValue: string | number | null;
+          let value: string | number | null;
+          if (
+            historyRecord.action == "creationTime" ||
+            historyRecord.action == "placingInBinTime"
+          ) {
+            oldValue =
+              historyRecord.oldValue === null ? null : (historyRecord.oldValue as Date).getTime();
+            value = historyRecord.value === null ? null : (historyRecord.value as Date).getTime();
+          } else {
+            oldValue = historyRecord.oldValue as string | null;
+            value = historyRecord.value as string | null;
+          }
+          return (
+            <RoomHistoryRecord
+              key={historyRecord.id}
+              action={historyRecord.action}
+              actionMakerUsername={historyRecord.user ? historyRecord.user.username : ""}
+              dateMillis={historyRecord.date.getTime()}
+              oldValue={oldValue}
+              value={value}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
--- client/components/rooms/room/roomUsers/UsersHistoryList.tsx
+++ client/components/rooms/room/roomUsers/UsersHistoryList.tsx
@@ -1,12 +1,14 @@
 import listenUsersHistoryRecords from "client/api/history/usersHistory/listenUsersHistoryRecords.api";
+import backToTopButtonStyles from "client/components/rooms/room/backToTopButton.module.scss";
 import DEFAULT_LARGE_HORIZONTAL_ALIGNMENT from "client/constants/defaultLargeHorizontalAlignment.constant";
 import UsersHistory from "common/clientModels/historyModels/usersHistory.model";
 import ArchivedUser from "common/types/history/archivedUser.type";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import UsersHistoryRecord from "./UsersHistoryRecord";
 
 export default function UsersHistoryList() {
   const [historyRecords, setHistoryRecords] = useState<UsersHistory["history"]>([]);
+  const [hideBackToTopButton, setHideBackToTopButton] = useState(true);
 
   useLayoutEffect(() => {
     const usersHistoryRecordsSubscription = listenUsersHistoryRecords().subscribe(
@@ -17,43 +19,70 @@ export default function UsersHistoryList() {
     return () => usersHistoryRecordsSubscription.unsubscribe();
   }, []);
 
+  useEffect(() => {
+    function decideIfShowBackToTopButton() {
+      setHideBackToTopButton(window.scrollY < 300);
+    }
+
+    document.addEventListener("scroll", decideIfShowBackToTopButton);
+    return () => document.removeEventListener("scroll", decideIfShowBackToTopButton);
+  }, []);
+
   return historyRecords.length == 0 ? (
     <div className="mt-5">
       <h4 className="text-center">No history records.</h4>
       <h4 className="text-center">Invite someone!</h4>
     </div>
   ) : (
-    <ul
-      className={`list-group list-group-flush overflow-auto ${DEFAULT_LARGE_HORIZONTAL_ALIGNMENT}`}
-    >
-      {historyRecords.map((historyRecord) => {
-        let oldValue: string | null;
-        let value: string | null;
-        if (historyRecord.action == "users" || historyRecord.action == "userRemovedFromWorkspace") {
-          oldValue =
-            historyRecord.oldValue === null
-              ? null
-              : (historyRecord.oldValue as ArchivedUser).username;
-          value =
-            historyRecord.value === null ? null : (historyRecord.value as ArchivedUser).username;
-        } else if (historyRecord.action == "allInvitationsCancel") {
-          oldValue = null;
-          value = null;
-        } else {
-          oldValue = historyRecord.oldValue as string | null;
-          value = historyRecord.value as string | null;
+    <div>
+      <button
+        className={`btn btn-primary ${backToTopButtonStyles.backToTopButton}`}
+        style={{ display: hideBackToTopButton ? "none" : "block" }}
+        onClick={() =>
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          })
         }
-        return (
-          <UsersHistoryRecord
-            key={historyRecord.id}
-            action={historyRecord.action}
-            actionMakerUsername={historyRecord.user ? historyRecord.user.username : ""}
-            dateMillis={historyRecord.date.getTime()}
-            oldValue={oldValue}
-            value={value}
-          />
-        );
-      })}
-    </ul>
+      >
+        Back to top
+      </button>
+      <ul
+        className={`list-group list-group-flush overflow-auto ${DEFAULT_LARGE_HORIZONTAL_ALIGNMENT}`}
+      >
+        {historyRecords.map((historyRecord) => {
+          let oldValue: string | null;
+          let value: string | null;
+          if (
+            historyRecord.action == "users" ||
+            historyRecord.action == "userRemovedFromWorkspace"
+          ) {
+            oldValue =
+              historyRecord.oldValue === null
+                ? null
+                : (historyRecord.oldValue as ArchivedUser).username;
+            value =
+              historyRecord.value === null ? null : (historyRecord.value as ArchivedUser).username;
+          } else if (historyRecord.action == "allInvitationsCancel") {
+            oldValue = null;
+            value = null;
+          } else {
+            oldValue = historyRecord.oldValue as string | null;
+            value = historyRecord.value as string | null;
+          }
+          return (
+            <UsersHistoryRecord
+              key={historyRecord.id}
+              action={historyRecord.action}
+              actionMakerUsername={historyRecord.user ? historyRecord.user.username : ""}
+              dateMillis={historyRecord.date.getTime()}
+              oldValue={oldValue}
+              value={value}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
