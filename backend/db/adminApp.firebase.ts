be276cbf073017d7ca68c3193b7ac6eae0988f04
Konrad Kasprzyk <konradwp2135@gmail.com>
2024-03-27 21:01:24 +0100

    Update initializing firebase admin app.

--- backend/db/adminApp.firebase.ts
+++ backend/db/adminApp.firebase.ts
@@ -26,6 +26,7 @@ const credential = USE_LOCAL_EMULATOR
       token_uri: process.env.TOKEN_URI,
       auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
       client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+      universe_domain: process.env.UNIVERSE_DOMAIN,
     };
 
 const adminApp =
